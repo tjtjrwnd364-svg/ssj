5c818848a42ee1ad08e802e127dfa71ff7793e90
DW-203 <DW-203@DESKTOP-RH6BQNG>
2026-01-05 09:02:26 +0900

    주차요금

--- java_control/src/study20260102/MainMethod.java
+++ java_control/src/study20260102/MainMethod.java
@@ -3,8 +3,82 @@ package study20260102;
 public class MainMethod {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		
+		if( 10 > 5) {
+			System.out.println(" 5보다 크다 ");
+			
+		}else {
+			System.out.println(" 5보다 작다 ");
+		}
 
+		
+		
+		// 바이킹 탑승 가능 : 145Cm 이상
+		
+		int tall = 160;
+		if (tall >= 145) {
+			System.out.println(" 탑승 가능 ");  //코드를 만들때 if를 긴문장 else를 짧은문장으로 유도
+		}else {
+			System.out.println(" 탑승 불가능 ");
+		}
+		
+		
+		 int age = 33;
+		 if( (age/10)==3 )                 //(age >= 30 && age < 40) 
+			 System.out.println(" 30대 이다 ");
+		 else 
+			 System.out.println(" 30대가 아니다 "); 
+		
+		 // 과제 
+		 // 주차장에 요금이 기본요금은 1000원이다.
+		 // 기본시간은 30분이다
+		 // 10분 당 100원식 추가 예) 39분은 1000원 40분은 1100원
+		 // 분을 입력시 요금이 얼마인지 출력하세요
+		 // 단 4시간 이상 주차시 기본요금은 2500원이다.
+		 // 예) 3시간 50분 (230분) -> 3000원
+		 //   4시간 12분 (252분) -> 2600원     
+		 
+		
+		
 	}
 
 }
+
+
+/* 제어문
+   
+ 조건문 - if  ,  switch
+ ==switch문은 선택하는것      -큰틀
+ ==if문은 참, 거짓 (~이라면)   -작은틀
+
+ if(조건식) {
+    조건식이 참일경우 실행할 코드;
+ }
+2
+  if(조건식) {
+     조건식이 참일 경우 실행할 코드;
+     }else{
+     조건식이 거짓일 경우 실행 할 코드;
+     }
+     
+3.
+  if(조건식) {
+      조건식1이 참인 경우에 실행
+       }else{
+      조건식1이 거짓인 경우에 실행
+       if(조건식2가 참인경우에 실행
+       }
+    }
+    
+       
+4.
+  if( 조건식1) {
+     조건식이 1이 참인경우 실행
+     }else if (조건식2) {
+        조건식2가 참인경우 실행  
+     }
+
+**조건식은 비교연산 논리연산 두가지로만 만든다
+
+*/
---
+++ java_control/src/study20260102/Parking.java
@@ -0,0 +1,45 @@
+// 과제 
+		 // 주차장에 요금이 기본요금은 1000원이다.
+		 // 기본시간은 30분이다
+		 // 10분 당 100원식 추가 예) 39분은 1000원 40분은 1100원
+		 // 분을 입력시 요금이 얼마인지 출력하세요
+		 // 단 4시간 이상 주차시 기본요금은 2500원이다.
+		 // 예) 3시간 50분 (230분) -> 3000원
+		 //   4시간 12분 (252분) -> 2600원     
+package study20260102;
+
+ import java.util.Scanner;
+
+public class Parking {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int minute;
+        int money = 0;
+
+        System.out.print("주차 시간(분) 입력: ");
+        minute = sc.nextInt();
+
+        if (minute >= 240) {
+            money = 2500;
+
+            int overTime = minute - 240;
+            int addMoney = (overTime / 10) * 100;
+
+            money = money + addMoney;
+        } else {
+            money = 1000;
+
+            if (minute > 30) {
+                int overTime = minute - 30;
+                int addMoney = (overTime / 10) * 100;
+
+                money = money + addMoney;
+            
+            }
+        }
+
+        System.out.println("요금: " + money + "원");
+    }
+}
